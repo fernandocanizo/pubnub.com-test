107175a8818d70c393bf6e8cf132af4398389b8e
Fernando L. Canizo <fernandocanizo@gmail.com>
2016-06-13 11:12:26 -0400

    Add PubNub and Twitter Bootstrap libs
    - Basic UI
    - Add "use strict"

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
+"use strict";
+
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Test PubNub.com' });
 });
 
 module.exports = router;
